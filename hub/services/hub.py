4fd3bd8d870f09d4511e2e6607129c6c3d02f48c
dipika <bagavedipika1423@gmail.com>
2022-08-26 14:40:33 +0530

    colr shade changed and fields added in mapping

--- hub/services/hub.py
+++ hub/services/hub.py
@@ -8,8 +8,7 @@ class HubService:
 
     @staticmethod
     def get_insights(reesponse_obj: InsightsSerializer):
-        query_data = Hub.objects.filter(starttime=[reesponse_obj.start_date, reesponse_obj.end_date]).values(*reesponse_obj.model_group_map).order_by().annotate(events=Sum('events'))
-        print(f"query_data{query_data}")
+        query_data = Hub.objects.filter(starttime__gte=reesponse_obj.start_date, endtime__lte=reesponse_obj.end_date).values(*reesponse_obj.model_group_map).order_by().annotate(events=Sum('events'))
         reesponse_obj.set_requst_queryset(query_data)
         incidents = Hub.objects.filter(starttime__gte=reesponse_obj.start_date, endtime__lte=reesponse_obj.end_date).values('criticality').order_by().annotate(events=Sum('events'))
         for row in incidents:
