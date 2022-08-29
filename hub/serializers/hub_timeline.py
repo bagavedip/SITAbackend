7ef569c2f9e04a206c3a5918310b10ee48a80675
Shashi Shekharam <98512424+ShekharamShashi@users.noreply.github.com>
2022-08-29 15:56:21 +0530

    shashi changes

--- hub/serializers/hub_timeline.py
+++ hub/serializers/hub_timeline.py
@@ -1,8 +1,10 @@
 from hub.constants.filter_map import Map
 
+
 class HubTimeline:
     def __init__(self, request) -> None:
         self.request_filters = []
+        self.header_filters = []
         self.model_group_map = []
         self.queryset = None
         self.datasets = []
@@ -13,5 +15,9 @@ class HubTimeline:
         self.end_date = filter_data.get('toDate')
         self.request_filters.append((filter_data.get('filterOptions').get('headerOption')))
         index = 1
+        filters = filter_data.get('filterOptions').get('headerFilters')
+        for filter in filters:
+            self.header_filters.append(filter)
         for filter in self.request_filters:
             self.model_group_map = self.model_group_map + Map.get_filter(filter).split(',')
+           
