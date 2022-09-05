8e768647471f49db38298def08645dfc7e5c79bb
dipika <bagavedipika1423@gmail.com>
2022-09-05 15:46:16 +0530

    clean code of user app and shared_app

--- shared_stage/models/rules.py
+++ shared_stage/models/rules.py
@@ -1,13 +1,14 @@
 from django.db import models
 from .usecase import UseCase
+from django.utils.translation import gettext_lazy as _
 
 
 class Rule(models.Model):
     """
-    Model to hold data for SOAR data
+    Model to hold data for Rule data
     """
-    rule_name = models.CharField(max_length=200, help_text="Rule Name")
-    description = models.CharField(max_length=200, help_text="Description")
-    default = models.CharField(max_length=200, help_text="Default")
-    usecase = models.ForeignKey(UseCase, on_delete = models.CASCADE, related_name="+")
+    rule_name = models.CharField(_("rule_name"), max_length=200, help_text=_("Rule Name"))
+    description = models.CharField(_("description"), max_length=200, help_text="Description")
+    default = models.CharField(_("default"), max_length=200, help_text="Default")
+    usecase = models.ForeignKey(UseCase, verbose_name=_("usecase"), on_delete=models.CASCADE, related_name="+")
     
--- users/services/user.py
+++ users/services/user.py
@@ -1,10 +1,9 @@
-from django.contrib.auth import get_user_model
-
-from users.models import User
 import logging
+from django.contrib.auth import get_user_model
 
 logger = logging.getLogger(__name__)
 User = get_user_model()
+
 
 class UserService:
     """
