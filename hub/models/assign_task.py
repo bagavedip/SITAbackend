6b569e7859a37968440a65d5e30cb9aeb22f01a0
dipika <bagavedipika1423@gmail.com>
2022-09-07 17:33:13 +0530

    clean code

--- hub/models/assign_task.py
+++ hub/models/assign_task.py
@@ -7,4 +7,4 @@ class AssignTask(models.Model):
      Model for Assign Task
     """
     incident_id = models.CharField(_("incident_id"), max_length=200, unique=True, help_text="incident_id")
-    assigned_user = models.CharField(_("assign_user"), max_length=200, help_text=_("assign_user"))
+    assigned_user = models.CharField(_("assign_user"), max_length=200, null=True, help_text=_("assign_user"))
