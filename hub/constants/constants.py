9fcdff2c9e926afecb709a7d3830b21e5129777c
Shashi Shekharam <98512424+ShekharamShashi@users.noreply.github.com>
2022-09-22 18:02:18 +0530

    changed for Incident details, start date, end date

--- hub/constants/constants.py
+++ hub/constants/constants.py
@@ -9,9 +9,9 @@ hire_del = 'HIREDEL'
 INSIGHT_TABLE_HEADER = {
     'INCIDENT': 'soar_id',
     'USE CASE': 'use_case',
-    'TICKET DETAILS': 'description',
-    'INCIDENT START TIME': 'starttime',
-    'INCIDENT END TIME': 'endtime',
+    'INCIDENT DETAILS': 'description',
+    'START DATE': 'starttime',
+    'END DATE': 'endtime',
     'STATUS': 'status',
     'LEVEL': 'priority',
     'OWNER': 'group'
