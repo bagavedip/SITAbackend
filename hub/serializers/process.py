6298e5b4c7fbfd798365e9f59f5679567c91e425
Shashi Shekharam <98512424+ShekharamShashi@users.noreply.github.com>
2022-09-24 18:03:13 +0530

    function added to serializer

--- hub/serializers/process.py
+++ hub/serializers/process.py
@@ -6,10 +6,13 @@ class ProcessSerializer(serializers.ModelSerializer):
     """
     Model serializer for Process information
     """
-
+    
+    function_name = serializers.CharField(source='function_id.function_name')
+    
     class Meta:
         model = Process
         fields = (
             "process",
+            "function_name",
         )
 
