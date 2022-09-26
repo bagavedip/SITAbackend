adc583268f7b311a828fb4adb6174c8bab170922
Shashi Shekharam <98512424+ShekharamShashi@users.noreply.github.com>
2022-09-26 13:07:32 +0530

    end date added

--- hub/models/process.py
+++ hub/models/process.py
@@ -10,6 +10,7 @@ class Process(models.Model):
     process = models.CharField(_("process"), max_length=50, null=True, help_text="Process Name")
     function_id = models.ForeignKey(Function, verbose_name=_("function_id"), on_delete=models.CASCADE,
                                     help_text=_("Function Name"))
+    end_date = models.DateField(_("end_date"), null=True, help_text=_("Delete Date"))
     
     def __str__(self):
         return self.process
