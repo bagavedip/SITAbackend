6c7a0b83a5a99f8aa9f4e991d0f3d27b27afdffb
Shashi Shekharam <98512424+ShekharamShashi@users.noreply.github.com>
2022-09-26 13:06:28 +0530

    end date added

--- hub/models/functions.py
+++ hub/models/functions.py
@@ -11,6 +11,7 @@ class Function(models.Model):
     function_name = models.CharField(_("function_name"), max_length=50, null=True, help_text=_("Function Name"))
     location_id = models.ForeignKey(GeoLocation, verbose_name=_("localtion_id"), on_delete=models.CASCADE,
                                     related_name="+")
+    end_date = models.DateField(_("end_date"), null=True, help_text=_("Delete Date"))
     
     def __str__(self):
         return self.function_name
