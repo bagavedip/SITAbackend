be486fa26e7fcca3b226c83d565f31e07cac5dc0
Shashi Shekharam <98512424+ShekharamShashi@users.noreply.github.com>
2022-09-26 13:06:02 +0530

    end date added

--- hub/models/entity.py
+++ hub/models/entity.py
@@ -8,6 +8,7 @@ class Entity(models.Model):
     """
     id = models.BigAutoField(_("id"), primary_key=True)
     entityname = models.CharField(_("entityname"), max_length=50, null=True, help_text=_("Entity Name"))
+    end_date = models.DateField(_("end_date"), null=True, help_text=_("Delete Date"))
     
     def __str__(self):
         return self.entityname
