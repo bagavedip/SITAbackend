5f12f7d4464bc2cc282533308d29823204df78ef
Shashi Shekharam <98512424+ShekharamShashi@users.noreply.github.com>
2022-09-26 13:04:57 +0530

    end date added

--- hub/models/category.py
+++ hub/models/category.py
@@ -8,6 +8,7 @@ class Category(models.Model):
     """
     id = models.BigAutoField(_("id"), primary_key=True)
     category = models.CharField(_("category"), max_length=50, null=True, help_text=_("Category Name"))
+    end_date = models.DateField(_("end_date"), null=True, help_text=_("Delete Date"))
     
     def __str__(self):
         return self.category
