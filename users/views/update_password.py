d458eee0f559bc13f0203dc09ed4662453fdf76d
Shashi Shekharam <98512424+ShekharamShashi@users.noreply.github.com>
2022-10-11 19:53:11 +0530

    Changes according to Payload

--- users/views/update_password.py
+++ users/views/update_password.py
@@ -2,6 +2,7 @@ import logging
 from django.db import transaction
 from django.contrib.auth.hashers import make_password
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import AuthenticationFailed
 
 from rest_framework import mixins, viewsets, status
 from rest_framework.response import Response
@@ -16,30 +17,34 @@ class PasswordUpdate(mixins.CreateModelMixin, viewsets.GenericViewSet):
 
     def update_password(self, request):
         request_user = request.data
-        serializer = PasswordUpdateSerializer(data = request_user)
+        request_data = {
+            "email":request_user["email"],
+            "oldPassword":request_user["oldPassword"],
+            "password":request_user["newPassword"]
+            }
+        serializer = PasswordUpdateSerializer(data = request_data)
         serializer.is_valid(raise_exception=True)
         validated_data = serializer.validated_data
         with transaction.atomic():
             password = make_password(validated_data["password"])
             if serializer.is_valid():
                 updated_user = User.objects.get(email = validated_data["email"])
+                if not updated_user.check_password(request_user["oldPassword"]):
+                    logger.error("Password verification failed.")
+                    raise AuthenticationFailed
                 updated_user.password  = password
                 updated_user.save()
                 return Response(
                     {
-                    "Data":serializer.data, 
-                    "Status":status.HTTP_201_CREATED,
-                    "Message": "Password Updated Successfully!!"
+                    "status":"SUCCESS",
+                    "message": "Password Reset Successfully!!"
                     }
                 )
             else:
                 return Response(
                     {
                     "Data":serializer.data, 
-                    "Status":status.HTTP_304_NOT_MODIFIED,
-                    "Message": "Oops! Something went wrong."
+                    "status":"FAIL",
+                    "message": "Oops! Something went wrong."
                     }
                 )
-        # updated_user.save()
-        # return Response({
-        #     "Message": "User Updated Successfully"})
