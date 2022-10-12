9e4f433c994e5c852b7e4da259430fe49de8ace7
Shashi Shekharam <98512424+ShekharamShashi@users.noreply.github.com>
2022-10-12 18:36:00 +0530

    changes for dataLabelColor

--- hub/serializers/oei_serializers.py
+++ hub/serializers/oei_serializers.py
@@ -130,11 +130,13 @@ class OeiSerializer:
                 if '-' in item['name']:
                     id = item['name'].split("-")[0]
                 backgroundColor.append(ColorMap.get_color(self.datasets[0]['label'], id))
+                dataLabelColor = ColorMap.get_color(self.datasets[0]['label'],id)
         self.datasets[0]['data'] = data
         self.datasets[0]['originalData'] = originalData
         self.datasets[0]['hierarchy'] = hierarchy
         self.datasets[0]['labels'] = labels
         self.datasets[0]['backgroundColor'] = backgroundColor
+        self.datasets[0]['dataLabelColor'] = dataLabelColor
         self.datasets[0]['spacing'] = 30
         self.datasets[0]['weight'] = 8
 
@@ -163,11 +165,13 @@ class OeiSerializer:
                     if '-' in child_item['name']:
                         id = child_item['name'].split("-")[1]
                     backgroundColor.append(ColorMap.get_color(self.datasets[1]['label'], id))
+                    dataLabelColor = ColorMap.get_color(self.datasets[1]['label'],id)
         self.datasets[1]['data'] = data
         self.datasets[1]['originalData'] = originalData
         self.datasets[1]['hierarchy'] = hierarchy
         self.datasets[1]['labels'] = labels
         self.datasets[1]['backgroundColor'] = backgroundColor
+        self.datasets[1]['dataLabelColor'] = dataLabelColor
         self.datasets[1]['spacing'] = 30
         self.datasets[1]['weight'] = 8
 
@@ -202,11 +206,13 @@ class OeiSerializer:
                         if '-' in child2_item['name']:
                             id = child2_item['name'].split("-")[0]
                         backgroundColor.append(ColorMap.get_color(self.datasets[2]['label'], id))
+                        dataLabelColor = ColorMap.get_color(self.datasets[2]['label'],id)
         self.datasets[2]['data'] = data
         self.datasets[2]['originalData'] = originalData
         self.datasets[2]['hierarchy'] = hierarchy
         self.datasets[2]['labels'] = labels
         self.datasets[2]['backgroundColor'] = backgroundColor
+        self.datasets[2]['dataLabelColor'] = dataLabelColor
         self.datasets[2]['spacing'] = 15
         self.datasets[2]['weight'] = 2
 
@@ -247,11 +253,13 @@ class OeiSerializer:
                             if '-' in child3_item['name']:
                                 id = child2_item['name'].split("-")[0]
                             backgroundColor.append(ColorMap.get_color(self.datasets[3]['label'], id))
+                            dataLabelColor = ColorMap.get_color(self.datasets[3]['label'],id)
         self.datasets[3]['data'] = data
         self.datasets[3]['originalData'] = originalData
         self.datasets[3]['hierarchy'] = hierarchy
         self.datasets[3]['labels'] = labels
         self.datasets[3]['backgroundColor'] = backgroundColor
+        self.datasets[2]['dataLabelColor'] = dataLabelColor
         self.datasets[3]['spacing'] = 15
         self.datasets[3]['weight'] = 2
 
