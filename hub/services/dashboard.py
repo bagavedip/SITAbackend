ab8d17f3a88548206d32643375646033831cbb30
dipika <bagavedipika1423@gmail.com>
2022-10-22 13:03:00 +0530

    Date Change with Proper Format

--- hub/services/dashboard.py
+++ hub/services/dashboard.py
@@ -10,4 +10,17 @@ class DashboardService:
     @staticmethod
     def dashboard_grid_data(response_obj: SecurityPulseGridSerializer):
         query_data = SecurityPulse.objects.all().values(*response_obj.select_cols).order_by('-updated_at')[:3]
+        response_data = {
+            "IsExternal": True,
+            "Title": "",
+            "index_id": "",
+            "Links": ""
+        }
+        for query in query_data:
+            response_data = {
+                "IsExternal": False,
+                "Title": query.main_title,
+                "index_id": "",
+                "Links": ""
+            }
         return query_data
