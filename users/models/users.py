66b844b3a8be8a0e20645640908f6dcf77cf60ff
dipika <bagavedipika1423@gmail.com>
2022-10-28 16:59:50 +0530

    insight chart changes

--- users/models/users.py
+++ users/models/users.py
@@ -17,9 +17,9 @@ class User(AbstractUser):
     key = models.BinaryField(_("key"),null=True)
     role_id = models.ForeignKey(Role, null=True, on_delete=models.CASCADE,
                                     help_text=_("Role Name"))
-    phone_code = models.CharField(_("phone code"), max_length=10)
-    phone_number = models.CharField(_("phone number"), max_length=20)
-    profile_photo = models.FileField(_("profile_photo"), upload_to='profile_photos', help_text="profile_photo")
+    phone_code = models.CharField(_("phone code"), max_length=10, null=True)
+    phone_number = models.CharField(_("phone number"), max_length=20, null=True)
+    profile_photo = models.FileField(_("profile_photo"), upload_to='profile_photos', help_text="profile_photo", default="")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['is_admin']  # Email & Password are required by default.
